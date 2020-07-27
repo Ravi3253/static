7aea3359546dea5e69b6ae4eae32cb5aeb114c79
ravitejakantayapalem <raviteja.kantayapalem@ncplinc.com>
2020-07-27 16:36:21 +0530

    no message

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get install -y python-certbot-apache
-sudo systemctl reload apache2
+sudo systemctl restart apache2
 sudo certbot --apache -d uth.today -m admin@ncplinc.com --agree-tos -n
 sudo service apache2 reload
